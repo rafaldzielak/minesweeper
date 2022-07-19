a68445462b539164366e908d5744a9e64e2d77ad
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-07-19 21:18:53 +0200

    Scoreboard component & story

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Scoreboard } from "./components/Scoreboard";
 import { Top } from "./components/Top/Top";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Top feature='Flag' firstAction='ctrl' secondAction='click'>
         Minesweeper
       </Top>
+      <Scoreboard time='000' levels={["beginner", "intermediate", "expert"]} mines='010' onReset={() => {}} />
     </div>
   );
 }
