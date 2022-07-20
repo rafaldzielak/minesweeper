51c899ab50de95b37e8ec90d23e50c8ca84b6216
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-07-20 21:07:55 +0200

    Mark @ as path for ./src

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
 });
