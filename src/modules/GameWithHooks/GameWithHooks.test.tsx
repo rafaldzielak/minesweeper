1ac2d55d63cc59190115ba301aa395514bbd1a5c
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-08-08 20:16:43 +0200

    useGame hook

--- src/modules/GameWithHooks/GameWithHooks.test.tsx
+++ src/modules/GameWithHooks/GameWithHooks.test.tsx
@@ -1,13 +1,6 @@
-import React from "react";
-import userEvent from "@testing-library/user-event";
-import { render, screen, waitFor } from "@testing-library/react";
-
-import { CellState } from "@/helpers/Field";
-
+import { render, screen } from "@testing-library/react";
 import { GameWithHooks } from "./GameWithHooks";
 import { describe, expect, it, vi } from "vitest";
-
-const { empty: e, hidden: h, bomb: b, flag: f } = CellState;
 
 vi.mock("@/helpers/Field");
 
@@ -17,23 +10,6 @@ describe("GameWithHooks test cases", () => {
       const { asFragment } = render(<GameWithHooks />);
       expect(screen.getAllByRole("cell")).toHaveLength(81);
       expect(asFragment()).toMatchSnapshot();
-    });
-
-    it("onChange game level handler", async () => {
-      render(<GameWithHooks />);
-      expect(screen.getAllByRole("cell")).toHaveLength(81);
-      userEvent.selectOptions(screen.getByRole("combobox"), "intermediate");
-      await waitFor(() => expect(screen.getAllByRole("cell")).toHaveLength(256));
-      userEvent.selectOptions(screen.getByRole("combobox"), "expert");
-      await waitFor(() => expect(screen.getAllByRole("cell")).toHaveLength(484));
-    });
-  });
-
-  describe("Open cell test cases", () => {
-    it("Open empty cell on the beginner level", async () => {
-      render(<GameWithHooks />);
-      await userEvent.click(screen.getByTestId("10_0,0"));
-      await waitFor(() => expect(screen.getAllByTestId(/^0_/)).toHaveLength(18));
     });
   });
 });
--- src/modules/GameWithHooks/GameWithHooks.tsx
+++ src/modules/GameWithHooks/GameWithHooks.tsx
@@ -1,43 +1,15 @@
-import React, { FC, useState, useMemo } from "react";
-
-import { Field, CellState, emptyFieldGenerator, fieldGenerator, Coords } from "@/helpers/Field";
-import { openCell } from "@/helpers/CellsManipulator";
-
-import { GameLevels, LevelNames, GameSettings } from "@/modules/GameSettings";
-
+import React, { FC } from "react";
+import { GameLevels, LevelNames } from "@/modules/GameSettings";
 import { Top } from "@/components/Top/Top";
 import { Scoreboard } from "@/components/Scoreboard";
 import { Grid } from "@/components/Grid/Grid";
 import { GameArea, Wrapper, GameOver } from "@/components/Game";
+import useGame from "./useGame";
 
 export const GameWithHooks: FC = () => {
-  const [level, setLevel] = useState<LevelNames>("beginner");
+  const { isGameOver, isWin, level, onChangeLevel, onClick, onReset, playerField, settings } = useGame();
 
-  const [size, bombs] = GameSettings[level];
-
-  const [playerField, setPlayerField] = useState<Field>(emptyFieldGenerator(size, CellState.hidden));
-  const [gameField, setGameField] = useState<Field>(fieldGenerator(size, bombs / (size * size)));
-
-  const onClick = (coords: Coords) => {
-    const newPlayerField = openCell(coords, playerField, gameField);
-    setPlayerField([...newPlayerField]);
-  };
-
-  const resetHandler = ([size, bombs]: [number, number]) => {
-    const newGameField = fieldGenerator(size, bombs / (size * size));
-    const newPlayerField = emptyFieldGenerator(size, CellState.hidden);
-
-    setGameField([...newGameField]);
-    setPlayerField([...newPlayerField]);
-  };
-
-  const onChangeLevel = ({ target: { value: level } }: React.ChangeEvent<HTMLSelectElement>) => {
-    setLevel(level as LevelNames);
-    const newSettings = GameSettings[level as LevelNames];
-    resetHandler(newSettings);
-  };
-
-  const onReset = () => resetHandler([size, bombs]);
+  const [, bombs] = settings;
 
   return (
     <Wrapper>
@@ -47,13 +19,15 @@ export const GameWithHooks: FC = () => {
       <GameArea>
         <Scoreboard
           time='0'
-          bombs='10'
+          bombs={String(bombs)}
           levels={GameLevels as unknown as string[]}
           defaultLevel={level}
-          onChangeLevel={onChangeLevel}
+          onChangeLevel={({ target: { value: level } }: React.ChangeEvent<HTMLSelectElement>) =>
+            onChangeLevel(level as LevelNames)
+          }
           onReset={onReset}
         />
-        <GameOver onClick={() => null} isWin={true} />
+        {isGameOver && <GameOver onClick={onReset} isWin={isWin} />}
         <Grid onClick={onClick} onContextMenu={() => null}>
           {playerField}
         </Grid>
