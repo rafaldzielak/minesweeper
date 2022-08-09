2dbcdf6933e4065b2d7c1317d6a579fb8c611f2d
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-08-09 20:12:10 +0200

    useGame hook (#7)

--- src/modules/GameWithHooks/GameWithHooks.test.tsx
+++ src/modules/GameWithHooks/GameWithHooks.test.tsx
@@ -1,13 +1,6 @@
-import React from "react";
-import userEvent from "@testing-library/user-event";
-import { render, screen, waitFor } from "@testing-library/react";
-
-import { CellState } from "@/helpers/Field";
-
+import { render, screen } from "@testing-library/react";
 import { GameWithHooks } from "./GameWithHooks";
 import { describe, expect, it, vi } from "vitest";
-
-const { empty: e, hidden: h, bomb: b, flag: f } = CellState;
 
 vi.mock("@/helpers/Field");
 
@@ -17,80 +10,6 @@ describe("GameWithHooks test cases", () => {
       const { asFragment } = render(<GameWithHooks />);
       expect(screen.getAllByRole("cell")).toHaveLength(81);
       expect(asFragment()).toMatchSnapshot();
-    });
-
-    it("onChange game level handler", async () => {
-      render(<GameWithHooks />);
-      expect(screen.getAllByRole("cell")).toHaveLength(81);
-      userEvent.selectOptions(screen.getByRole("combobox"), "intermediate");
-      await waitFor(() => expect(screen.getAllByRole("cell")).toHaveLength(256));
-      userEvent.selectOptions(screen.getByRole("combobox"), "expert");
-      await waitFor(() => expect(screen.getAllByRole("cell")).toHaveLength(484));
-    });
-  });
-
-  describe("Open cell test cases", () => {
-    it("Open empty cell on the beginner level", async () => {
-      render(<GameWithHooks />);
-      await userEvent.click(screen.getByTestId("10_0,0"));
-      await waitFor(() => expect(screen.getAllByTestId(/^0_/)).toHaveLength(18));
-    });
-
-    it("Check click to the cell when the level is changed", async () => {
-      render(<GameWithHooks />);
-      expect(screen.getAllByRole("cell")).toHaveLength(81);
-
-      userEvent.selectOptions(screen.getByRole("combobox"), "intermediate");
-      await waitFor(() => expect(screen.getAllByRole("cell")).toHaveLength(256));
-
-      userEvent.click(screen.getByTestId("10_15,15"));
-      await waitFor(() => expect(screen.getAllByTestId(/^0_/)).toHaveLength(2));
-
-      userEvent.selectOptions(screen.getByRole("combobox"), "expert");
-      await waitFor(() => expect(screen.getAllByRole("cell")).toHaveLength(484));
-
-      userEvent.click(screen.getByTestId("10_21,21"));
-      await waitFor(() => expect(screen.getAllByTestId(/^0_/)).toHaveLength(1));
-      await waitFor(() => expect(screen.getAllByTestId(/^1_/)).toHaveLength(2));
-      await waitFor(() => expect(screen.getAllByTestId(/^2_/)).toHaveLength(1));
-    });
-
-    it("onReset game handler", async () => {
-      render(<GameWithHooks />);
-      await waitFor(() => expect(screen.getAllByTestId(/^10_/)).toHaveLength(81));
-
-      userEvent.click(screen.getByTestId("10_0,8"));
-      await waitFor(() => expect(screen.getAllByTestId(/^1_/)).toHaveLength(1));
-
-      userEvent.click(screen.getByTestId("10_0,0"));
-      await waitFor(() => expect(screen.getAllByTestId(/^0_/)).toHaveLength(18));
-
-      userEvent.click(screen.getByRole("button"));
-      await waitFor(() => expect(screen.getAllByTestId(/^10_/)).toHaveLength(81));
-    });
-  });
-
-  describe("Game over behavior", () => {
-    it("Player loose the game", async () => {
-      render(<GameWithHooks />);
-
-      userEvent.click(screen.getByTestId("10_0,8"));
-      await waitFor(() => expect(screen.getAllByTestId(/^1_/)).toHaveLength(1));
-
-      userEvent.click(screen.getByTestId("10_0,0"));
-      await waitFor(() => expect(screen.getAllByTestId(/^0_/)).toHaveLength(18));
-
-      userEvent.click(screen.getByTestId("10_0,7"));
-      await waitFor(() => expect(screen.getByText("🙁")).toBeTruthy());
-
-      expect(screen.getAllByTestId(/^0_/)).toHaveLength(27);
-      expect(screen.getAllByTestId(/^1_/)).toHaveLength(30);
-      expect(screen.getAllByTestId(/^2_/)).toHaveLength(12);
-      expect(screen.getAllByTestId(/^3_/)).toHaveLength(2);
-
-      userEvent.click(screen.getByText("🙁"));
-      await waitFor(() => expect(screen.getAllByTestId(/^10_/)).toHaveLength(81));
-      await waitFor(() => expect(screen.queryByText("🙁")).toBeFalsy());
     });
   });
 });
--- src/modules/GameWithHooks/GameWithHooks.tsx
+++ src/modules/GameWithHooks/GameWithHooks.tsx
@@ -1,53 +1,15 @@
-import React, { FC, useState, useMemo } from "react";
-
-import { Field, CellState, emptyFieldGenerator, fieldGenerator, Coords } from "@/helpers/Field";
-import { openCell } from "@/helpers/CellsManipulator";
-
-import { GameLevels, LevelNames, GameSettings } from "@/modules/GameSettings";
-
+import React, { FC } from "react";
+import { GameLevels, LevelNames } from "@/modules/GameSettings";
 import { Top } from "@/components/Top/Top";
 import { Scoreboard } from "@/components/Scoreboard";
 import { Grid } from "@/components/Grid/Grid";
 import { GameArea, Wrapper, GameOver } from "@/components/Game";
+import useGame from "./useGame";
 
 export const GameWithHooks: FC = () => {
-  const [level, setLevel] = useState<LevelNames>("beginner");
-  const [isGameOver, setIsGameOver] = useState(false);
-  const [isWin, setIsWin] = useState(false);
+  const { isGameOver, isWin, level, onChangeLevel, onClick, onReset, playerField, settings } = useGame();
 
-  const [size, bombs] = GameSettings[level];
-
-  const [playerField, setPlayerField] = useState<Field>(emptyFieldGenerator(size, CellState.hidden));
-  const [gameField, setGameField] = useState<Field>(fieldGenerator(size, bombs / (size * size)));
-
-  const onClick = (coords: Coords) => {
-    try {
-      const newPlayerField = openCell(coords, playerField, gameField);
-      setPlayerField([...newPlayerField]);
-    } catch (error) {
-      setPlayerField([...gameField]);
-      setIsGameOver(true);
-      setIsWin(false);
-    }
-  };
-
-  const resetHandler = ([size, bombs]: [number, number]) => {
-    const newGameField = fieldGenerator(size, bombs / (size * size));
-    const newPlayerField = emptyFieldGenerator(size, CellState.hidden);
-
-    setGameField(newGameField);
-    setPlayerField(newPlayerField);
-    setIsGameOver(false);
-    setIsWin(false);
-  };
-
-  const onChangeLevel = ({ target: { value: level } }: React.ChangeEvent<HTMLSelectElement>) => {
-    setLevel(level as LevelNames);
-    const newSettings = GameSettings[level as LevelNames];
-    resetHandler(newSettings);
-  };
-
-  const onReset = () => resetHandler([size, bombs]);
+  const [, bombs] = settings;
 
   return (
     <Wrapper>
@@ -57,10 +19,12 @@ export const GameWithHooks: FC = () => {
       <GameArea>
         <Scoreboard
           time='0'
-          bombs='10'
+          bombs={String(bombs)}
           levels={GameLevels as unknown as string[]}
           defaultLevel={level}
-          onChangeLevel={onChangeLevel}
+          onChangeLevel={({ target: { value: level } }: React.ChangeEvent<HTMLSelectElement>) =>
+            onChangeLevel(level as LevelNames)
+          }
           onReset={onReset}
         />
         {isGameOver && <GameOver onClick={onReset} isWin={isWin} />}
