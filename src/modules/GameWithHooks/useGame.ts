3d3be637ddac9db27c84280d658284a5bda3b5bd
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-09-10 08:25:17 +0200

    useStatus and useSettings hooks (#17)

--- src/modules/GameWithHooks/useGame.ts
+++ src/modules/GameWithHooks/useGame.ts
@@ -3,8 +3,10 @@ import { useState, useEffect } from "react";
 import { Field, CellState, emptyFieldGenerator, fieldGenerator, Coords } from "@/helpers/Field";
 import { openCell } from "@/helpers/openCell";
 
-import { LevelNames, GameSettings } from "@/modules/GameSettings";
+import { LevelNames } from "@/modules/GameSettings";
 import { setFlag } from "@/helpers/setFlag";
+import { useStatus } from "./useStatus";
+import { useSettings } from "./useSettings";
 
 type ReturnType = {
   level: LevelNames;
@@ -23,14 +25,17 @@ type ReturnType = {
 };
 
 const useGame = (): ReturnType => {
-  const [level, setLevel] = useState<LevelNames>("beginner");
-  const [isGameOver, setIsGameOver] = useState(false);
-  const [isWin, setIsWin] = useState(false);
-  const [isGameStarted, setIsGameStarted] = useState(false);
+  const {
+    settings: [size, bombs],
+    level,
+    setLevel,
+  } = useSettings();
+
+  const { isGameStarted, isWin, isGameOver, setNewGame, setInProgress, setGameWin, setGameLoose } =
+    useStatus();
+
   const [time, setTime] = useState(0);
   const [flagCounter, setFlagCounter] = useState(0);
-
-  const [size, bombs] = GameSettings[level];
 
   const [playerField, setPlayerField] = useState<Field>(emptyFieldGenerator(size, CellState.hidden));
   const [gameField, setGameField] = useState<Field>(fieldGenerator(size, bombs / (size * size)));
@@ -42,27 +47,20 @@ const useGame = (): ReturnType => {
     return () => clearInterval(interval);
   }, [isGameStarted, isGameOver, setTime]);
 
-  const setGameOver = (isSolved = false) => {
-    setIsGameOver(true);
-    setIsWin(isSolved);
-  };
-
   const onClick = (coords: Coords) => {
-    if (!isGameStarted) setIsGameStarted(true);
+    if (!isGameStarted) setInProgress();
     try {
       const [newPlayerField, isSolved] = openCell(coords, playerField, gameField);
-      if (isSolved) setGameOver(isSolved);
-
+      if (isSolved) setGameWin();
       setPlayerField([...newPlayerField]);
     } catch (error) {
       setPlayerField([...gameField]);
-      setGameOver();
-      setIsWin(false);
+      setGameLoose();
     }
   };
 
   const onContextMenu = (coords: Coords) => {
-    if (!isGameStarted) setIsGameStarted(true);
+    if (!isGameStarted) setInProgress();
     const [newPlayerField, isSolved, newFlagCounter] = setFlag(
       coords,
       playerField,
@@ -71,7 +69,7 @@ const useGame = (): ReturnType => {
       bombs
     );
     setFlagCounter(newFlagCounter);
-    if (isSolved) setGameOver(isSolved);
+    if (isSolved) setGameWin();
     setPlayerField([...newPlayerField]);
   };
 
@@ -80,16 +78,13 @@ const useGame = (): ReturnType => {
     const newPlayerField = emptyFieldGenerator(size, CellState.hidden);
     setTime(0);
 
-    setIsGameStarted(false);
     setGameField(newGameField);
     setPlayerField(newPlayerField);
-    setIsGameOver(false);
-    setIsWin(false);
+    setNewGame();
   };
 
   const onChangeLevel = (level: LevelNames) => {
-    setLevel(level);
-    const newSettings = GameSettings[level];
+    const newSettings = setLevel(level);
     resetHandler(newSettings);
   };
 
