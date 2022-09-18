7a93cba35db4d7af8e01c309465a07522d761b2a
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-09-18 16:27:51 +0200

    Game with redux

---
+++ src/modules/GameWithRedux/GameWithRedux.tsx
@@ -0,0 +1,47 @@
+import React, { FC, useReducer, useCallback } from "react";
+import { GameLevels, LevelNames } from "@/modules/GameSettings";
+import { Scoreboard } from "@/components/Scoreboard";
+import { Grid } from "@/components/Grid/Grid";
+import { GameArea, Wrapper, GameOver } from "@/components/Game";
+import { reducer, actions, getInitialState } from "./game";
+import { Coords } from "@/helpers/Field";
+
+export const GameWithRedux: FC = () => {
+  const [{ isGameOver, isWin, level, playerField, settings, time, flagCounter }, dispatch] = useReducer(
+    reducer,
+    getInitialState()
+  );
+
+  const [, bombs] = settings;
+
+  const onChangeLevel = useCallback(
+    ({ target: { value: level } }: React.ChangeEvent<HTMLSelectElement>) =>
+      dispatch(actions.changeLevel(level as LevelNames)),
+    []
+  );
+
+  const onContextMenu = useCallback((coords: Coords) => dispatch(actions.setFlag(coords)), []);
+
+  const onReset = useCallback(() => dispatch(actions.reset()), []);
+
+  const onClick = useCallback((coords: Coords) => dispatch(actions.openCell(coords)), []);
+
+  return (
+    <>
+      <GameArea>
+        <Scoreboard
+          time={time}
+          bombs={bombs - flagCounter}
+          levels={GameLevels as unknown as string[]}
+          defaultLevel={level}
+          onChangeLevel={onChangeLevel}
+          onReset={onReset}
+        />
+        {isGameOver && <GameOver onClick={onReset} isWin={isWin} />}
+        <Grid onClick={onClick} onContextMenu={onContextMenu}>
+          {playerField}
+        </Grid>
+      </GameArea>
+    </>
+  );
+};
